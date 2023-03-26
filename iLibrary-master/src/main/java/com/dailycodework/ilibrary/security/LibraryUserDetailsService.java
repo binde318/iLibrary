19d7803d0756bf91d69cb906d4b754811516e400
NANNIM BINDE <59848588+binde318@users.noreply.github.com>
2023-03-26 10:25:37 +0100

    Add files via upload
    The iLibrary_ work

---
+++ iLibrary-master/src/main/java/com/dailycodework/ilibrary/security/LibraryUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.dailycodework.ilibrary.security;
+
+import com.dailycodework.ilibrary.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibraryUserDetailsService  implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(LibraryUserDetails::new).orElseThrow(() -> new UsernameNotFoundException("No User Found"));
+    }
+}
